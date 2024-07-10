b19e733426ccf6c4c0b80f1a2126d8a37100bd83
myst <johndaveaquino35@gmail.com>
2024-07-10 17:22:05 +0800

    Update InputTextUtil.java

--- src/main/java/org/guanzon/autoapp/utils/InputTextUtil.java
+++ src/main/java/org/guanzon/autoapp/utils/InputTextUtil.java
@@ -6,6 +6,7 @@ package org.guanzon.autoapp.utils;
 
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
+import javafx.beans.value.ChangeListener;
 import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
 
@@ -36,5 +37,24 @@ public class InputTextUtil {
                 textArea.setText(newValue.toUpperCase());
             }
         });
+    }
+
+    public static void addTextLimiter(TextField textField, int maxLength) {
+        // Remove any existing listeners to avoid stacking them
+        if (textField.getProperties().get("textLimiter") != null) {
+            textField.textProperty().removeListener((ChangeListener) textField.getProperties().get("textLimiter"));
+        }
+
+        // Create a new listener to limit the text length
+        ChangeListener<String> textLimiter = (observable, oldValue, newValue) -> {
+            if (newValue.length() > maxLength) {
+                textField.setText(oldValue);
+            }
+        };
+
+        // Add the new listener to the text field
+        textField.textProperty().addListener(textLimiter);
+        // Store the listener reference for future removal if necessary
+        textField.getProperties().put("textLimiter", textLimiter);
     }
 }
