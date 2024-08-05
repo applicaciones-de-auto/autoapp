193a55044a01bda51d2788c3b52d214b03b2be23
myst <johndaveaquino35@gmail.com>
2024-08-05 17:10:34 +0800

    Create VehicleInquiryFollowUpController.java

---
+++ src/main/java/org/guanzon/autoapp/controllers/sales/VehicleInquiryFollowUpController.java
@@ -0,0 +1,49 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/javafx/FXMLController.java to edit this template
+ */
+package org.guanzon.autoapp.controllers.sales;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.DatePicker;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
+
+/**
+ * FXML Controller class
+ *
+ * @author User
+ */
+public class VehicleInquiryFollowUpController implements Initializable {
+
+    @FXML
+    private Button btnSave;
+    @FXML
+    private Button btnClose;
+    @FXML
+    private DatePicker datePicker03;
+    @FXML
+    private ComboBox<?> comboBox02;
+    @FXML
+    private TextField txtField04;
+    @FXML
+    private TextArea textArea05;
+    @FXML
+    private TextArea textArea06;
+    @FXML
+    private TextField txtField01;
+
+    /**
+     * Initializes the controller class.
+     */
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        // TODO
+    }
+
+}
