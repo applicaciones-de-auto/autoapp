575b7b1cb19c86c5a2d3a8325745ded7e4be2f5a
myst <johndaveaquino35@gmail.com>
2024-08-05 17:11:11 +0800

    Create VehicleInquirySalesAdvancesController.java

---
+++ src/main/java/org/guanzon/autoapp/controllers/sales/VehicleInquirySalesAdvancesController.java
@@ -0,0 +1,52 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/javafx/FXMLController.java to edit this template
+ */
+package org.guanzon.autoapp.controllers.sales;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
+
+/**
+ * FXML Controller class
+ *
+ * @author User
+ */
+public class VehicleInquirySalesAdvancesController implements Initializable {
+
+    @FXML
+    private Button btnApply;
+    @FXML
+    private Button btnClose;
+    @FXML
+    private ComboBox<?> comboBox01;
+    @FXML
+    private TextField txtField02;
+    @FXML
+    private TextField txtField04;
+    @FXML
+    private TextArea textArea05;
+    @FXML
+    private TextField txtField06;
+    @FXML
+    private TextField txtField07;
+    @FXML
+    private TextField txtField08;
+    @FXML
+    private TextField txtField03;
+
+    /**
+     * Initializes the controller class.
+     */
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        // TODO
+    }
+
+}
