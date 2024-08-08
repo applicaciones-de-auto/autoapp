c7776e91ae803fb9e9227e35012cf35d293224ce
myst <johndaveaquino35@gmail.com>
2024-08-08 17:11:33 +0800

    Update ModelInquiryTestDrive.java

--- src/main/java/org/guanzon/autoapp/models/sales/ModelInquiryTestDrive.java
+++ src/main/java/org/guanzon/autoapp/models/sales/ModelInquiryTestDrive.java
@@ -4,7 +4,6 @@
  */
 package org.guanzon.autoapp.models.sales;
 
-import org.guanzon.autoapp.models.general.*;
 import javafx.beans.property.SimpleStringProperty;
 import javafx.scene.control.CheckBox;
 
@@ -16,12 +15,18 @@ public class ModelInquiryTestDrive {
 
     private SimpleStringProperty tblindex01;
     private CheckBox select;
-    private SimpleStringProperty tblindex02; //TestDrive Model
+    private SimpleStringProperty tblindex02; //Make ID
+    private SimpleStringProperty tblindex03; //Make Desc
+    private SimpleStringProperty tblindex04; //Model ID
+    private SimpleStringProperty tblindex05; //Model Desc
 
-    public ModelInquiryTestDrive(String tblindex01, String tblindex02) {
+    public ModelInquiryTestDrive(String tblindex01, String tblindex02, String tblindex03, String tblindex04, String tblindex05) {
         this.tblindex01 = new SimpleStringProperty(tblindex01);
         this.select = new CheckBox();
         this.tblindex02 = new SimpleStringProperty(tblindex02);
+        this.tblindex03 = new SimpleStringProperty(tblindex03);
+        this.tblindex04 = new SimpleStringProperty(tblindex04);
+        this.tblindex05 = new SimpleStringProperty(tblindex05);
     }
 
     public String getTblindex01() {
@@ -46,6 +51,30 @@ public class ModelInquiryTestDrive {
 
     public void setTblindex02(String tblindex02) {
         this.tblindex02.set(tblindex02);
+    }
+
+    public String getTblindex03() {
+        return tblindex03.get();
+    }
+
+    public void setTblindex03(String tblindex03) {
+        this.tblindex03.set(tblindex03);
+    }
+
+    public String getTblindex04() {
+        return tblindex04.get();
+    }
+
+    public void setTblindex04(String tblindex04) {
+        this.tblindex04.set(tblindex04);
+    }
+
+    public String getTblindex05() {
+        return tblindex05.get();
+    }
+
+    public void setTblindex05(String tblindex05) {
+        this.tblindex05.set(tblindex05);
     }
 
 }
